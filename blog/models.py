c98a9e135c4d62f22c2f876f1609323770912631
ccav5 <516877910@qq.com>
2020-05-22 07:37:17 +0800

    5_22

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 #自定义管理器
@@ -24,8 +25,16 @@ class Post(models.Model):
     objects=models.Manager()
     published=PublishedManager()
 
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                        args=[self.publish.year,
+                              self.publish.month,
+                              self.publish.day,
+                              self.slug])
 
-    class Meta():
+
+
+    class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
