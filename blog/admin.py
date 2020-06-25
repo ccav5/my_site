9a51321de70bf854c312306201bcbf009f900cb8
ccav5 <516877910@qq.com>
2020-06-25 21:29:21 +0800

    6-25 home

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post,Comment
 @admin.register(Post) #将postAdmin 类注册成post的管理类
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title','slug','author','publish','status',)
@@ -11,3 +11,10 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'publish'
     ordering = ('status','publish')
 # Register your models here.
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name','email','created','active')
+    list_filter = ('active','created','updated')
+    search_fields = ('name','email','body')
+
