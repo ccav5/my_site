d4d10a23d0367a94fae27a877936b280171d818a
ccav5 <516877910@qq.com>
2020-06-26 23:06:33 +0800

    home 6-26

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,9 @@ from .models import Post,Comment
 from django.views.generic import ListView
 from .forms import EmailPostForm,CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
+from django.db.models import Count
+
 
 # Create your views here.
 class PostListView(ListView):
@@ -12,17 +15,21 @@ class PostListView(ListView):
     paginate_by = 2
     template_name = 'blog/post/list.html'
 
-# def post_list(request):
-#     object_list = Post.published.all()
-#     paginator = Paginator(object_list,2)
-#     page = request.GET.get('page')
-#     try:
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
-#     return render(request,'blog/post/list.html',{'page':page,'posts':posts})
+def post_list(request,tag_slug=None):
+    object_list = Post.published.all()
+    tag=None
+    if tag_slug:
+        tag = get_object_or_404(Tag,slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+    paginator = Paginator(object_list,2)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request,'blog/post/list.html',{'page':page,'posts':posts,'tag':tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -31,21 +38,25 @@ def post_detail(request, year, month, day, post):
                             publish__year = year,
                            publish__month = month,
                             publish__day =  day)
-    comments = post.comments.filter(active = True)
+    comments = post.comments.filter(active=True)
     new_comment = None
 
     if request.method == 'POST':
-        comment_form =CommentForm(data = request.POST)
+        comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             new_comment = comment_form.save(commit=False)
             new_comment.post = post
             new_comment.save()
     else:
         comment_form = CommentForm()
+    post_tags_ids = post.tags.values_list('id',flat=True)
+    similar_tags = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_tags.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
 
     return render(request,'blog/post/detail.html',{'post':post,'comments':comments,
                                                    'new_comment':new_comment,
-                                                   'comment_form':comment_form})
+                                                   'comment_form':comment_form,
+                                                   'similar_posts':similar_posts})
 
 
 def post_share(request, post_id):
